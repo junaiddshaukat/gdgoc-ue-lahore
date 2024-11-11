e3e3259a96bb6c1442c653bb786b49706003dd93
Junaid <junaiddshaukat@gmail.com>
2024-11-11 23:05:32 +0500

    file structure changes

--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-        domains: ['hebbkx1anhila5yf.public.blob.vercel-storage.com','res.cloudinary.com','avatar.iran.liara.run']
-}
-}
-
-export default nextConfig;
